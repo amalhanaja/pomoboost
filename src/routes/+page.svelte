1709f46c8759e40ddf201d3d50933e95b7154ad7
amalhanaja <amalhanaja@gmail.com>
2023-07-17 08:09:07 +0700

    MVP 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,10 +22,10 @@
 <header>
 	<NavBar onSettingClick={() => (settingsOpen = true)} />
 </header>
-<main>
-	<PomoTimer pomoTimerStore={pomoTimer} settings={$timerSettings} pomodoroCountStore={pomodoroCount}/>
-	<h1 class="text-3xl font-bold underline">
-		Welcome to SvelteKit {$timerSettings?.longBreakDuration}
-	</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class=" mt-16">
+	<PomoTimer
+		pomoTimerStore={pomoTimer}
+		settings={$timerSettings}
+		pomodoroCountStore={pomodoroCount}
+	/>
 </main>
