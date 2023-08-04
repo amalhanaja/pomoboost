cb981155e70c04327218c95f4637b6efe37a2749
amalhanaja <amalhanaja@gmail.com>
2023-08-05 04:13:54 +0700

    feat(setting): notifcation volume control

--- src/lib/components/Settings/NotificationSettings.svelte
+++ src/lib/components/Settings/NotificationSettings.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-
 </script>
 
 <section class="flex flex-col">
-    Notification Settings 
+	<div class="form-control mt-2 flex flex-row items-center">
+		<label for="volume" class="label font-semibold label-text flex-1">Volume</label>
+        <span class="mr-2">40</span>
+		<input id="volume" type="range" min="0" max="100" value="25" class="flex-1 range range-xs" step="1" />
+	</div>
 </section>
