f4c8695f538eaaaf2726e27551ecd2ba80cc2277
amalhanaja <amalhanaja@gmail.com>
2023-08-18 01:40:17 +0700

    feat(pwa): fix node env

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,8 +1,6 @@
 import { build, files, prerendered, version } from '$service-worker';
 import { precacheAndRoute } from 'workbox-precaching';
 
-console.log(process.env.NODE_ENV);
-
 const precacheList = [...build, ...files, ...prerendered].map((file) => ({
 	url: file,
 	revision: version
